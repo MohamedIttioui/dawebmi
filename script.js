9cd20f75887414e504e4269a9fb672fbe01d30f1
Mohamed Ittioui <mittioui20721@iesjoanramis.org>
2025-11-10 17:43:19 +0100

    Versió inicial del projecte web

---
+++ script.js
@@ -0,0 +1,8 @@
+let count = 0;
+const button = document.getElementById('btnCambiar');
+const display = document.getElementById('textoDiv');
+
+button.addEventListener('click', () => {
+  count++;
+  display.textContent = `Botó premut: ${count} cops`;
+});
